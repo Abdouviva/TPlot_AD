aac5abfd53ee77642c9e5a8e27838d7b17880a7e
Abdouviva <168718203+Abdouviva@users.noreply.github.com>
2024-05-02 11:08:13 +0200

    Create secrets.h.ino

---
+++ secrets.h/secrets.h.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+const char* ssid = ‘dz2024’ ;
+const char* password = ‘123vivaalgerie’ ;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
